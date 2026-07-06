2d162365b5ca6a80418b7d05c5743b6cd438fce3
suraj-pandey-208 <surajpandey0289@gmail.com>
2026-07-06 23:48:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://realchat-backend-ok0x.onrender.com"
 import { Provider } from "react-redux"
 import { store, persistor } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
